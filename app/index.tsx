fce300224d949a5602a7f6bc661ab0663224aba0
Wil481liam <williamdias481@gmail.com>
2026-08-27 10:41:46 -0300

    Adicionando os botões da tela de login e textos

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={styles.container}
     >
-      <Text>Usuario:</Text>
-      <TextInput style={styles.input} placeholder="Usuario"/>
-      <Text>Senha</Text>
-      <TextInput style={styles.input} placeholder="Senha" secureTextEntry/>
+      <View style={styles.campos}>
+        <Text>Usuario:</Text>
+        <TextInput style={styles.input} placeholder="Usuario"/>
+      </View>
+
+      <View style={styles.campos}>
+        <Text>Senha:</Text>
+        <TextInput style={styles.input} placeholder="Senha" secureTextEntry/>
+
+        <Pressable onPress={() => alert("Recuperar senha")}>
+          <Text style={styles.link}>Esqueci minha senha</Text>
+        </Pressable>
+
+      </View>
+
+      <View style={styles.campos}>
+        <Pressable style={styles.botao} onPress={() => {}}>
+          <Text style={styles.textoBotao}>Entrar</Text>
+        </Pressable>
+
+        <View style={styles.campoCriarConta}>
+          <Text style={styles.textoCriarConta}>
+            Ainda não tem uma conta?
+          </Text>
+
+          <Pressable onPress={() => alert("Criar conta")}>
+            <Text style={styles.linkCriarConta}>Criar conta</Text>
+          </Pressable>
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -20,9 +47,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     
   },
+
+  campos: {
+    width: "80%",
+    marginBottom: "10%",
+  },
   
   input:{
-     width: 250,
+    width: "100%",
     height: 40,
 
     borderWidth: 1,
@@ -30,5 +62,52 @@ const styles = StyleSheet.create({
     borderRadius: 5,
 
     paddingHorizontal: 10,
-  }
+  },
+
+  botao: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#4CAF50",
+    borderRadius: 5,
+
+    
+
+    justifyContent: "center",
+    alignItems: "center",
+
+    marginTop: 10
+  },
+
+  textoBotao: {
+    color: "#fff",
+  },
+
+  link: {
+    color: "#0a52cd",
+    textDecorationLine: "underline",
+    fontSize: 14,
+    marginTop: 15,
+
+    alignSelf: "flex-end",
+  },
+
+  linkCriarConta: {
+    color: "#4CAF50",
+    fontSize: 14,
+    fontWeight: "600",
+  },
+
+  campoCriarConta: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  textoCriarConta: {
+    color: "#666",
+    fontSize: 14,
+  },
+
+
 });
