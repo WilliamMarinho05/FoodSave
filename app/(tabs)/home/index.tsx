51a50b29afaf7ba9b57fd1cf4e2f38401f9238b2
Wil481liam <williamdias481@gmail.com>
2026-09-06 19:54:06 -0300

    Implementando o painel de validades com um use state

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import PainelValidade from "@/src/components/PainelValidade";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
+  // O estado fica na Home para que o botão consiga alterá-lo
+  const [painel, setPainel] = useState({
+    noPrazo: 12,
+    atencao: 3,
+    vencidos: 2,
+  });
+
+  const handleCadastrarAlimento = () => {
+    // Incrementa +1 no card "NO PRAZO"
+    setPainel((prev) => ({
+      ...prev,
+      noPrazo: prev.noPrazo + 1,
+    }));
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bem-vindo ao FoodSave!</Text>
-      <Text>Essa é a tela inicial.</Text>
+      <PainelValidade painel={painel} />
+
+      <Pressable style={styles.botaoCadastrar} onPress={handleCadastrarAlimento}>
+        <Text style={styles.textoBotao}>+ Cadastrar novo alimento</Text>
+      </Pressable>
+
     </View>
   );
 }
@@ -12,12 +33,22 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
   },
 
-  title: {
-    fontSize: 24,
+  botaoCadastrar: {
+    marginTop: 25,
+    backgroundColor: "#4CAF50",
+    paddingVertical: 14,
+    paddingHorizontal: 24,
+    borderRadius: 10,
+    width: "90%",
+    alignItems: "center",
+  },
+  textoBotao: {
+    color: "#FFF",
+    fontSize: 16,
     fontWeight: "bold",
   },
 });
